15bf3e4cb35cd6bcf9868f6ec89fe07cc4102797
EvanRupert <rupertevanr@gmail.com>
2017-10-07 19:12:11 -0400

    New TeleBot creation style

--- index.js
+++ index.js
@@ -1,24 +1,8 @@
 'use strict'
 
-var express = require('express');
-var app     = express();
+const TeleBot = require('telebot');
+const bot = new TeleBot('457195654:AAHVNzh7SVXQr1wpLKw75x_7h_snj1IlA5Y');
 
-app.set('port', (process.env.PORT || 5000));
-
-const TeleBot = require('telebot')
-
-const bot = new TeleBot(
-{
-	token: '457195654:AAHVNzh7SVXQr1wpLKw75x_7h_snj1IlA5Y', // Required. Telegram Bot API token.
-    webhook: { // Optional. Use webhook instead of polling.
-        key: 'key.pem', // Optional. Private key for server.
-        cert: 'cert.pem', // Optional. Public key.
-        url: 'https://....', // HTTPS url to send updates to.
-        host: '0.0.0.0', // Webhook server host.
-        port: 5000, // Server port.
-        maxConnections: 40 // Optional. Maximum allowed number of simultaneous HTTPS connections to the webhook for update delivery
-    }
-});
 
 bot.on('text', (msg) => msg.reply.text(msg.text));
 

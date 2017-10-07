1d1d5d3a55126bb7a2c289e61253055a3325e72d
EvanRupert <rupertevanr@gmail.com>
2017-10-07 18:59:59 -0400

    Replaced polling with webhook

--- index.js
+++ index.js
@@ -10,12 +10,14 @@ const TeleBot = require('telebot')
 const bot = new TeleBot(
 {
 	token: '457195654:AAHVNzh7SVXQr1wpLKw75x_7h_snj1IlA5Y', // Required. Telegram Bot API token.
-	polling: { // Optional. Use polling.
-		interval: 1000, // Optional. How often check updates (in ms).
-		timeout: 0, // Optional. Update polling timeout (0 - short polling).
-		limit: 100, // Optional. Limits the number of updates to be retrieved.
-		retryTimeout: 5000, // Optional. Reconnecting timeout (in ms).
-	}
+    webhook: { // Optional. Use webhook instead of polling.
+        key: 'key.pem', // Optional. Private key for server.
+        cert: 'cert.pem', // Optional. Public key.
+        url: 'https://....', // HTTPS url to send updates to.
+        host: '0.0.0.0', // Webhook server host.
+        port: 5000, // Server port.
+        maxConnections: 40 // Optional. Maximum allowed number of simultaneous HTTPS connections to the webhook for update delivery
+    }
 });
 
 bot.on('text', (msg) => msg.reply.text(msg.text));

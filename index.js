011ca3eda9de0b289e94dcccbe0a74727df2b568
katahar <rishibasdeo@gmail.com>
2017-10-08 00:15:05 -0400

    Finally got startup commands working

--- index.js
+++ index.js
@@ -1,18 +1,43 @@
-'use strict'
+'use strict'; 
 
-const TeleBot = require('telebot')
+const TeleBot = require('telebot');
+//var setup = require('./SetupController.js');
 
-const bot = new TeleBot(
-{
-	token: '457195654:AAHVNzh7SVXQr1wpLKw75x_7h_snj1IlA5Y', // Required. Telegram Bot API token.
-	polling: { // Optional. Use polling.
-		interval: 1000, // Optional. How often check updates (in ms).
-		timeout: 0, // Optional. Update polling timeout (0 - short polling).
-		limit: 100, // Optional. Limits the number of updates to be retrieved.
-		retryTimeout: 5000, // Optional. Reconnecting timeout (in ms).
-	}
+const bot = new TeleBot({
+   token: '457195654:AAHVNzh7SVXQr1wpLKw75x_7h_snj1IlA5Y', // Required. Telegram Bot API token. 
+   polling: { // Optional. Use polling. 
+	   interval: 1000, // Optional. How often check updates (in ms). 
+	   timeout: 0, // Optional. Update polling timeout (0 - short polling). 
+	   limit: 100, // Optional. Limits the number of updates to be retrieved. 
+	   retryTimeout: 5000,// Optional. Reconnecting timeout (in ms). 
+	   usePlugins: ['askUser']
+   }
+       
 });
+	
+	bot.on(/^\/name (.+)$/, (msg, props) => {
+		let name = props.match[1];
+		const id = msg.chat.id
+		bot.sendMessage(id, name)
+		bot.setChatTitle(id, name)
 
-bot.on('text', (msg) => msg.reply.text(msg.text));
+	//bot.on(['/start','/hello'], (msg) => {
+	//	bot.sendMessage(msg.from.id, 'Please enter your event name')
+	});
 
-bot.start();
+	bot.on(/^\/location (.+)$/, (msg, props) => {
+		let location = props.match[1];
+		bot.sendMessage(msg.from.id, location)
+	});
+
+	bot.on(/^\/date (.+)$/, (msg, props) => {
+		let date = props.match[1];
+		bot.sendMessage(msg.from.id, date)
+	});
+
+	
+
+	
+
+
+	bot.start();
